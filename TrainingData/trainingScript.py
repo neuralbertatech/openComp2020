562e3a23f2ba5b40fc0791d97487b8f671d8bd1b
cameronhildebrandt <cameronhildebrandt1@gmail.com>
2020-11-08 15:45:47 -0700

    fixed conflicts

--- TrainingData/trainingScript.py
+++ TrainingData/trainingScript.py
@@ -9,29 +9,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 DataCounter = 501
-<<<<<<< HEAD
 StartingNumber = 1000#680
 SamplesToRecord = 0#20
-=======
-StartingNumber = 180
-SamplesToRecord = 20
->>>>>>> 1c80e3685df64a0f763022f676447e3a938b6ba8
 EndingNumber = -1 # assign to -1 to be automatically determined by SamplesToRecord
 TestSubject = "Cameron"
 TestFeature = "Limbs"
 FileNumber = [StartingNumber, StartingNumber, StartingNumber, StartingNumber, StartingNumber, StartingNumber, StartingNumber, StartingNumber]
 FileTypes = ["Up", "Down", "Forward", 'Back', "Left", "Right", "Stay", "Jaw"]
 CurrFileType = 0
-<<<<<<< HEAD
 LastFileType = 0
-GeneratePlots = True
+GeneratePlots = False
 MyFile = open(TestSubject + TestFeature + "/" + FileTypes[CurrFileType] + str(FileNumber[CurrFileType]) + ".txt", 'w')
-=======
-MyFile = open(TestSubject + TestFeature + "/" + FileTypes[CurrFileType] + str(FileNumber[CurrFileType]) + ".txt", 'w')
-
-if(EndingNumber == -1):
-    EndingNumber = StartingNumber + SamplesToRecord
->>>>>>> 1c80e3685df64a0f763022f676447e3a938b6ba8
 
 if(EndingNumber == -1):
     EndingNumber = StartingNumber + SamplesToRecord
@@ -39,8 +27,6 @@ if(EndingNumber == -1):
 # def add_input(input_queue):
 #     while True:
 #         input_queue.put(sys.stdin.read(1))
-
-<<<<<<< HEAD
 
 
 def plotSample(filePath):
@@ -64,41 +50,6 @@ def plotSample(filePath):
     x = np.linspace(0,len(channels[0])-1, len(channels[0]))
 
     plotColors = ["#ff0000", "#ff0038", "#ff0060", "#ff0085", "#ff26a9", "#ff4aca", "#ee65e7", "#d47bff", "#b68fff", "#92a0ff", "#6aaeff", "#38bbff", "#00c5ff", "#00ceff", "#00d6ff", "#00dcff"]
-=======
-    if(DataCounter < 250): #250 samples = 1 second (250Hz)
-        dataPoint = var.channels_data
-        dataPoint.append(str(FileTypes[CurrFileType]))
-
-        MyFile.write(str(dataPoint) + "\n")
-        MyFile.flush()
-        DataCounter += 1
-
-    else:
-        if(DataCounter > 500): # First run, ignore it
-            DataCounter = 0
-            CurrFileType = random.randint(0,7)
-
-        else:
-            FileNumber[CurrFileType] += 1
-
-            CurrFileType = random.randint(0,7)
-
-            print("Current Samples: ")
-            print(FileNumber)
-
-            if(all(i > EndingNumber for i in FileNumber)):
-                exit()
-
-            while(FileNumber[CurrFileType] > EndingNumber):
-                CurrFileType = random.randint(0,7)
-
-            DataCounter = 0
-            MyFile = open(TestSubject + TestFeature + "/" + FileTypes[CurrFileType] + str(FileNumber[CurrFileType]) + ".txt", 'w')
-
-            print("### " + FileTypes[CurrFileType] + " ###")
-            time.sleep(1)
-            print("Recording.")
->>>>>>> 1c80e3685df64a0f763022f676447e3a938b6ba8
 
     for channel in range(len(channels)):
         plt.plot(x, channels[channel], plotColors[channel])
